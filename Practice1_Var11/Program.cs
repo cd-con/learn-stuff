187c5e09c7dd8adc406229c25e4fcf8d2babb02b
NullPointerException <61092992+cd-con@users.noreply.github.com>
2022-09-16 16:06:40 +0300

    Практическая #1 на солях

---
+++ Practice1_Var11/Program.cs
@@ -0,0 +1,42 @@
+﻿// Практическая #1 вариант #11
+
+// Возвращает массив с числами, полученными из ввода пользователя
+double[] getUserInput()
+{
+    Console.Write("Введите числа через точку с запятой:");
+    string[] rawUserInput = Console.ReadLine().Split(";"); // Получаем "сырой" ввод пользователя в виде массива строк
+    double[] userInput = new double[rawUserInput.Length]; // Создаём массив чисел, равный по длине массиву ввода
+
+    // Конвертируем массив строк в массив чисел
+    for (int i = 0; i < rawUserInput.Length; i++)
+    {
+        userInput[i] = double.Parse(rawUserInput[i]);
+    }
+
+    return userInput; // Возвращаем
+}
+
+// Сортировка пузыриком?
+double[] sort(double[] sortArray)
+{
+    for (int iter = 0; iter < sortArray.Length; iter++) // Проходим по массиву
+    {
+        for (int i = sortArray.Length - 1; i > 0; i--) // Оперируем с последним числом, двигаем его пока оно не будет меньше следущего
+        {
+            if (sortArray[i] > sortArray[i - 1])
+            {
+                // Метод 3 кружек :)
+                double tempNum = sortArray[i - 1];
+                sortArray[i - 1] = sortArray[i];
+                sortArray[i] = tempNum;
+            }
+        }
+    }
+    return sortArray; // Возвращаем
+}
+
+// Вывод
+foreach (double item in sort(getUserInput()))
+{
+    Console.Write(item.ToString() + " ");
+}
