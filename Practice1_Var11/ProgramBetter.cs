217429e55853f9a0d978518d584c2d7d4b7d3450
cd-con <61092992+cd-con@users.noreply.github.com>
2022-09-21 10:21:16 +0300

    Add files via upload

---
+++ Practice1_Var11/ProgramBetter.cs
@@ -0,0 +1,22 @@
+﻿// Практическая #1 вариант #11
+
+// Возвращает массив с числами, полученными из ввода пользователя
+double[] getUserInput()
+{
+    Console.Write("Введите числа через точку с запятой: ");
+    string[] rawUserInput = Console.ReadLine().Split(";"); // Получаем "сырой" ввод пользователя в виде массива строк
+
+    return rawUserInput.Select(s => double.Parse(s)).ToArray(); ; // Возвращаем
+}
+
+// Сортировка пузыриком
+double[] bubbleSortInverted(double[] sortArray)
+{
+    Array.Sort(sortArray);
+
+    return sortArray.Reverse().ToArray(); // Возвращаем
+}
+
+
+// Вывод
+Console.Write(string.Join(" ", bubbleSortInverted(getUserInput())));
