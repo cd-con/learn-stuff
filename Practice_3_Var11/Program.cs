06c4da191cfcf40c658696963384d9b0b2c254cf
cd-con <61092992+cd-con@users.noreply.github.com>
2022-10-05 09:35:33 +0300

    Add files via upload

---
+++ Practice_3_Var11/Program.cs
@@ -0,0 +1,7 @@
+﻿double result = 0;
+for (double n = 2; n <= 100; n++)
+{
+    result += 1 / ((n - 1) * (n + 1));
+}
+
+Console.WriteLine("Результат вычисления: " + result + " - Проверка: " + 3d/4d + " - Погрешность: " + Math.Abs(3d / 4d - result) + " - Входит ли число в предел погрешности? " + (Math.Abs(3d/4d - result) < 0.1d));
