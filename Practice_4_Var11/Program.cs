07a19f5aea6bc180f5c25161115886d6d3ce429d
cd-con <61092992+cd-con@users.noreply.github.com>
2022-10-28 15:20:20 +0300

    Add files via upload

---
+++ Practice_4_Var11/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practice_4_Var11;
+
+int.TryParse(Console.ReadLine(), out int x);
+
+int fibChain2 = 0;
+int fibChain1 = 1;
+int fibChain0 = 1;
+
+while (fibChain0 < x)
+{
+    Console.WriteLine(fibChain0);
+    fibChain0 = fibChain2 + fibChain1;
+    (fibChain2, fibChain1) = (fibChain1, fibChain0);
+
+}
+Console.WriteLine(fibChain0);
+Console.Write($"Число {x}");
+Console.Write(fibChain0 == x ? " находится в последовательности Фиббоначи" : " не находится в последовательности Фиббоначи");
---
+++ Practice_5_Var11/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите К >> ");
+int.TryParse(Console.ReadLine(), out int k);
+
+int chainElement;
+float minDistanceBetween2Nums = k;
+int result = 0;
+
+do
+{
+    int.TryParse(Console.ReadLine(), out chainElement);
+    if (chainElement == k)
+    {
+        Console.WriteLine("Нельзя ввести элемент последовательности, равный K");
+        continue;
+    }
+    float dist = MathF.Abs(k - chainElement);
+    result = minDistanceBetween2Nums > dist?chainElement:result;
+    minDistanceBetween2Nums = minDistanceBetween2Nums > dist ? dist : minDistanceBetween2Nums;
+
+} while (chainElement != 1000);
+
+Console.WriteLine($"Число {k} ближе всего к числу {result}");
