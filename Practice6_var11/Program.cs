0c4fdd072bc88bb12e442860abf16663185e776e
cd-con <61092992+cd-con@users.noreply.github.com>
2022-11-02 09:52:03 +0300

    Add files via upload

---
+++ Practice6_var11/Program.cs
@@ -0,0 +1,16 @@
+﻿for (int num = 300; num <= 600; num++)
+{
+	int sum = 0;
+	for (int dividerNum = 1; dividerNum < num; dividerNum++)
+	{
+		if (num % dividerNum == 0)
+		{
+            sum += dividerNum;
+        }			
+	}
+
+	if (sum % 10 == 0)
+	{
+		Console.WriteLine($"Число {num} имеет сумму делителей, кратную 10 ({sum})");
+	}
+}
