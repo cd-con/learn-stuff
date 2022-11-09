af6a88570353db38bdd638606af479ea470265a9
cd-con <61092992+cd-con@users.noreply.github.com>
2022-11-09 09:31:09 +0300

    Ура 7 практос
    Вернее 9, ну да похуй

---
+++ Practice_7_Var11/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите числа в массив череез запятую >>");
+string input = Console.ReadLine();
+int[] arr = input.Split(",").ToList().Select(num => int.Parse(num)).ToArray();
+
+float result = 0;
+int resCounter = 0;
+foreach (int item in arr)
+{
+    if (item < 0)
+    {
+        resCounter++;
+        result += item;
+    }
+}
+
+result /= resCounter;
+Console.WriteLine(result);
