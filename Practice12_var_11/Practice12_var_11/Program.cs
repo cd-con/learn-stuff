de934ef123060dbbc083f854982a300ce16c4ca5
cd-con <61092992+cd-con@users.noreply.github.com>
2022-12-15 14:35:04 +0300

    12 и 13 практическая
    Уже идут по порядку. Нумерация более не нарушается

---
+++ Practice12_var_11/Practice12_var_11/Program.cs
@@ -0,0 +1,16 @@
+﻿int numSum = 0;
+int numCount = 0;
+string userInput = Console.ReadLine();
+
+foreach (char item in userInput)
+{
+	if (char.IsDigit(item))
+	{
+		numCount++;
+		numSum += int.Parse(item.ToString());
+		userInput = userInput.Replace(item.ToString(), item + $"({10 - int.Parse(item.ToString())})");
+	}
+}
+Console.WriteLine($"В введёной строке найдено: {numCount} чисел, общей суммой {numSum}");
+Console.WriteLine($"Итоговая строка: {userInput}");
+
---
+++ Practice_13_Var11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вводим путь
+Console.Write("Введите путь до файла: ");
+string path = Console.ReadLine();
+
+// Решение
+using (StreamReader sr = new(new FileStream(path, FileMode.Open)))
+{
+    string readLine;
+    int bCount = 0;
+    int gCount = 0;
+    while ((readLine = sr.ReadLine()) != null)
+	{
+        bCount += readLine.Count(c => c.ToString().ToLower() == "б");
+        gCount += readLine.Count(c => c.ToString().ToLower() == "г");
+	}
+    Console.WriteLine($"В данном тексте было найдено {bCount} букв 'Б' и {gCount} букв 'Г'");
+}
