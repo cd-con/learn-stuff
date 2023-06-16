b9ced42c6315fc810393e89c1579b955d4be0ee7
Rustam Yusubov <61092992+cd-con@users.noreply.github.com>
2023-06-16 10:26:03 +0300

    15 практос

---
+++ LibForPractice15_var11/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LibForPractice15_var11
+{
+    public class ClassExample
+    {
+        /// <summary>
+        /// Проверяет, является ли число чётным
+        /// </summary>
+        /// <param name="a">Число</param>
+        /// <returns>True - число чётное; False - нечётное</returns>
+        public bool IsEven(int a)
+        {
+            return a % 2 == 0;
+        }
+        /// <summary>
+        /// Проверяет, оканчивается ли число единицей
+        /// </summary>
+        /// <param name="a">Число</param>
+        /// <returns>True - число оканчивается на единицу; False - нет</returns>
+        public bool IsEndingWith1(int a) 
+        {
+            return a % 10 == 1; 
+        }
+        /// <summary>
+        /// Проверяет, начинается ли число с двойки
+        /// </summary>
+        /// <param name="a">Число</param>
+        /// <returns>True - число начинается с двойки; False - нет</returns>
+        public bool IsStartingWith2(int a)
+        {
+            return (int)(a / Math.Pow(10, (int)Math.Floor(Math.Log10(a)))) == 2;
+        }
+    }
+}
---
+++ Practice15_var11/Program.cs
@@ -0,0 +1,9 @@
+﻿using LibForPractice15_var11;
+
+ClassExample myClass = new();
+
+Console.WriteLine(myClass.IsEven(int.Parse(Console.ReadLine())));
+Console.WriteLine(myClass.IsEndingWith1(int.Parse(Console.ReadLine())));
+Console.WriteLine(myClass.IsStartingWith2(int.Parse(Console.ReadLine())));
+
+
