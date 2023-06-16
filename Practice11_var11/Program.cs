f166ec42afcebd847cd5b13522fe42b93bf45302
Rustam Yusubov <61092992+cd-con@users.noreply.github.com>
2023-06-16 10:22:32 +0300

    Практос со строками

---
+++ Practice11_var11/Program.cs
@@ -0,0 +1,22 @@
+﻿string userInput = Console.ReadLine();
+char[] nums = new char[10];
+
+// массив цыферок от 0 до 10
+for (int i = 0; i < 10; i++)
+{
+    nums[i] = i.ToString()[0];
+}
+
+int numCounter = 0;
+int numSumCounter = 0;
+foreach (char c in userInput)
+{
+    if (nums.Contains(c))
+    {
+        numCounter++;
+        numSumCounter += int.Parse(c.ToString());
+        userInput = userInput.Replace(c.ToString(),c + $"({10- int.Parse(c.ToString())})");
+    }
+}
+Console.WriteLine($"Всего чисел: {numCounter}; Сумма: {numSumCounter}");
+Console.WriteLine(userInput);
